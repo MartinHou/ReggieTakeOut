3f7fd07fdba400b0241afbecea9cd7fd7c059e17
martinhou <martinhou@foxmail.com>
2022-08-16 13:54:33 +0800

    MVC静态资源映射，创建启动文件

--- src/main/java/com/martin/reggie/ReggieApplication.java
+++ src/main/java/com/martin/reggie/ReggieApplication.java
@@ -1,5 +1,14 @@
 package com.martin.reggie;
 
-public class ReggieApplication {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j  // 开启日志,任何类上加此注解,都可在类中使用log进行控制台输出
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("项目启动成功");
+    }
 }
