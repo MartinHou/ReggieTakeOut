bc283ffaa2c6948ad377f8d4ace82cc766c30bb3
martinhou <martinhou@foxmail.com>
2022-08-25 09:32:41 +0800

    分类分页查询功能

--- src/main/java/com/martin/reggie/controller/CategoryController.java
+++ src/main/java/com/martin/reggie/controller/CategoryController.java
@@ -1,14 +1,13 @@
 package com.martin.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.martin.reggie.common.R;
 import com.martin.reggie.entitty.Category;
 import com.martin.reggie.service.CategoryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -27,5 +26,18 @@ public class CategoryController {
         log.info("category:{}",category.toString());
         categoryService.save(category);
         return R.success("新增分类成功");
+    }
+
+    @GetMapping("/page")
+    public R<Page<Category>> page(int page, int pageSize) {
+        //分页构造器
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //根据sort条件排序
+        queryWrapper.orderByAsc(Category::getSort);
+        //调用Service
+        categoryService.page(pageInfo,queryWrapper);
+        return R.success(pageInfo);
     }
 }
--- src/main/java/com/martin/reggie/controller/EmployeeController.java
+++ src/main/java/com/martin/reggie/controller/EmployeeController.java
@@ -91,11 +91,11 @@ public class EmployeeController {
      * 员工信息分页查询
      */
     @GetMapping("/page")
-    public R<Page> page(int page, int pageSize, String name){
+    public R<Page<Employee>> page(int page, int pageSize, String name){
         log.info("page={},pagesize={},name={}",page,pageSize,name);
 
         //创建分页器
-        Page pageInfo = new Page(page,pageSize);
+        Page<Employee> pageInfo = new Page<>(page,pageSize);
         //创建条件构造器
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
         //添加name相关条件
